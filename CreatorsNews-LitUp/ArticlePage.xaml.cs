bddd327afef5b9cacf37d250c336168d206aa646
Daren May <daren.may@gmail.com>
2017-06-16 13:39:02 -0600

    Adding the "lit up" version

---
+++ CreatorsNews-LitUp/ArticlePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CreatorsNews
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ArticlePage : Page
+    {
+        public ArticlePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var articleIndex = (int) e.Parameter;
+            var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
+            if (animation != null)
+            {
+                Image.Opacity = 0;
+                // Wait for image opened. In future Insider Preview releases, this won't be necessary.
+                Image.ImageOpened += (sender_, e_) =>
+                {
+                    Image.Opacity = 1;
+                    animation.TryStart(Image);
+                };
+            }
+
+            ViewModel.LoadArticle(articleIndex);
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Frame.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("Image", Image);
+
+            // Add a fade out effect
+            Transitions = new TransitionCollection();
+            Transitions.Add(new ContentThemeTransition());
+        }
+
+    }
+}
---
+++ CreatorsNews-LitUp/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CreatorsNews
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static int? _navigatedIndex;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Extend the app into the titlebar so that we can apply acrylic
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void ArticleClicked(object sender, ItemClickEventArgs e)
+        {
+            var articleIndex = Array.IndexOf(ViewModel.Articles, e.ClickedItem);
+
+            var container = ArticlesGridView.ContainerFromItem(e.ClickedItem) as GridViewItem;
+            if (container != null)
+            {
+                var root = (FrameworkElement)container.ContentTemplateRoot;
+                var image = (UIElement)root.FindName("Image");
+
+                ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("Image", image);
+            }
+
+            // Add a fade out effect
+            Transitions = new TransitionCollection();
+            Transitions.Add(new ContentThemeTransition());
+
+            Frame.Navigate(typeof(ArticlePage), _navigatedIndex = articleIndex);
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Don't use vertical entrance animation with connected animation
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                EntranceTransition.FromVerticalOffset = 0;
+            }
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Frame.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void ArticlesGridView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (_navigatedIndex != null)
+            {
+                // May be able to perform backwards Connected Animation
+                var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("Image");
+                if (animation != null)
+                {
+                    var item = ViewModel.Articles[_navigatedIndex.Value];
+
+                    ArticlesGridView.ScrollIntoView(item, ScrollIntoViewAlignment.Default);
+                    ArticlesGridView.UpdateLayout();
+
+                    var container = ArticlesGridView.ContainerFromItem(item) as GridViewItem;
+                    if (container != null)
+                    {
+                        var root = (FrameworkElement)container.ContentTemplateRoot;
+                        var image = (Image)root.FindName("Image");
+
+                        // Wait for image opened. In future Insider Preview releases, this won't be necessary.
+                        //image.Opacity = 0;
+                        animation.TryStart(image);
+
+                        //image.ImageOpened += (sender_, e_) =>
+                        //{
+                        //    image.Opacity = 1;
+                        //    animation.TryStart(image);
+                        //};
+                    }
+                    else
+                    {
+                        animation.Cancel();
+                    }
+                }
+
+                _navigatedIndex = null;
+            }
+        }
+    }
+}
